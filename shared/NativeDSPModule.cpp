e9a10720ef16a521a68e6a19adc673155ec748b9
Braulio Ríos <braulioriosf@gmail.com>
2024-12-06 14:24:57 -0300

    Copy kissfft lib, working call to kiss_fft()

--- shared/NativeDSPModule.cpp
+++ shared/NativeDSPModule.cpp
@@ -1,4 +1,5 @@
 #include "NativeDSPModule.h"
+#include "kiss_fft.h"
 
 namespace facebook::react {
 
@@ -6,6 +7,24 @@ NativeDSPModule::NativeDSPModule(std::shared_ptr<CallInvoker> jsInvoker)
     : NativeDSPModuleCxxSpec(std::move(jsInvoker)) {}
 
 std::string NativeDSPModule::reverseString(jsi::Runtime& rt, std::string input) {
+    kiss_fft_cfg cfg = kiss_fft_alloc(1024, 0, nullptr, nullptr);
+    if (!cfg) {
+        return "error";
+    }
+
+    kiss_fft_cpx fin[1024];
+    kiss_fft_cpx fout[1024];
+    
+    // Fill input with data (example)
+    for (int i = 0; i < 1024; i++) {
+        fin[i].r = i;  // Real part
+        fin[i].i = 0;  // Imaginary part
+    }
+
+    // Perform FFT
+    kiss_fft(cfg, fin, fout);
+    kiss_fft_free(cfg);
+
   return std::string(input.rbegin(), input.rend());
 }
 
