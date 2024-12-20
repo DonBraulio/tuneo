955e4b60abbc1757b29a7002c0a7f57368ff1b20
Braulio Ríos <braulioriosf@gmail.com>
2024-12-20 19:47:12 -0300

    Bugfix, octaves must start in C

--- src/MusicalNotes.ts
+++ src/MusicalNotes.ts
@@ -1,5 +1,5 @@
-const A4_FREQUENCY = 440.0 // Reference frequency for A4
-const NOTE_NAMES = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"] as const
+const A4_FREQUENCY = 440.0 // Reference frequency (A4)
+const NOTE_NAMES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"] as const
 const OCTAVE_NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 export type NoteName = (typeof NOTE_NAMES)[number]
@@ -14,15 +14,15 @@ export type Note = { name: NoteName; octave: OctaveNumber }
 export function getNoteFromFrequency(frequency: number): Note | undefined {
   if (frequency <= 0) return
 
-  // Calculate the number of semitones from A4
+  // Calculate the number of semitones from reference A4
   const semitonesFromA4 = 12 * Math.log2(frequency / A4_FREQUENCY)
 
-  // Round to the nearest semitone
-  const semitoneIndex = Math.round(semitonesFromA4)
+  // Octaves start in C, calculate semitones from C4
+  const semitonesFromC4 = Math.round(semitonesFromA4 + 9)
 
   // Determine the note and octave
-  const noteIndex = ((semitoneIndex % 12) + 12) % 12 // Ensure index is positive
-  const octave = 4 + Math.floor((semitoneIndex + 3) / 12) // Adjust octave
+  const noteIndex = ((semitonesFromC4 % 12) + 12) % 12
+  const octave = 4 + Math.floor(semitonesFromC4 / 12) // Adjust octave
 
   return { name: NOTE_NAMES[noteIndex], octave }
 }
@@ -35,12 +35,18 @@ export function getNoteFromFrequency(frequency: number): Note | undefined {
 export function getFrequencyFromNote(note?: Note): number {
   if (!note) return 0
 
-  // Find the index of the note name in the NOTE_NAMES array
-  const noteIndex = NOTE_NAMES.indexOf(note.name)
-
   // Calculate the semitone offset from A4
-  const semitonesFromA4 = (note.octave - 4) * 12 + noteIndex
+  const noteDistance = NOTE_NAMES.indexOf(note.name) - NOTE_NAMES.indexOf("A")
+  const semitonesFromA4 = (note.octave - 4) * 12 + noteDistance
 
-  // Calculate the frequency using the semitone formula
+  // freq = ref^(semitones / 12)
   return A4_FREQUENCY * Math.pow(2, semitonesFromA4 / 12)
+}
+
+export function getSineOfFrequency(frequency: number, sampleRate: number, bufSize: number) {
+  const sineWave: number[] = []
+  for (let i = 0; i < bufSize; i++) {
+    sineWave[i] = Math.sin((2 * Math.PI * i * frequency) / sampleRate)
+  }
+  return sineWave
 }
